cf41ab3de41da9e0f7e99018942b43b61db8e7e1
CSE506 Student Account <cse506@vl185.cs.stonybrook.edu>
2014-09-12 21:39:26 -0400

    abc

---
+++ ex1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+  int x = 1;
+  printf("Hello x = %d\n", x);
+  
+  //
+  // Put in-line assembly here to increment
+  // the value of x by 1 using in-line assembly
+  //
+      __asm__ ("movl %1, %0\n\t"
+               "addl %2, %0"
+               : "=r" (x)                      
+               : "r" (x), "r" (1) 
+               : "0");                          
+
+  printf("Hello x = %d after increment\n", x);
+
+  if(x == 2){
+    printf("OK\n");
+  }
+  else{
+    printf("ERROR\n");
+  }
+}
